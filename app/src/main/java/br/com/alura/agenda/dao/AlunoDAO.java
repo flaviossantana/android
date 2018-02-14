4b44ec5c5e3212113be9d65b5ab31a4f766115b5
flavio.santana <9090flavio@gmail.com>
2018-02-14 20:51:00 -0200

    Criação do DAO de Alunos.

--- app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -10,17 +10,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class AlunoDAO extends SQLiteOpenHelper {
 
-    public AlunoDAO(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private static final int NEW_VERSION = 1;
+    private static final int OLD_VERSION = 1;
+
+    public AlunoDAO(Context context) {
+        super(context, "agenda", null, NEW_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, endereço TEXT, telefone TEXT, site TEXT, nota REAL);";
+        db.execSQL(sql);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS Alunos";
+        db.execSQL(sql);
+        onCreate(db);
     }
 }
