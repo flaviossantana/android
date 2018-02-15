9bc4c6520a4e87b723673c29762cf9ebd0704ab4
FLAVIO.SANTANA <9090flavio@gmail.com>
2018-02-15 08:37:02 -0200

    [REVERT] Commit feito em outra maquina.

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -46,7 +46,7 @@ public class FormularioActivity extends AppCompatActivity {
 
                 Toast.makeText(FormularioActivity.this, aluno.getNome() +  " salvo com sucesso!", Toast.LENGTH_SHORT).show();
                 finish();
-                //break;
+                break;
         }
 
         return super.onOptionsItemSelected(item);
