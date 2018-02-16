1126694cc719f251271eb24f191ef8b6dfce90b4
FLAVIO.SANTANA <9090flavio@gmail.com>
2018-02-16 09:20:02 -0200

    Tratando eventos de sistema.

---
+++ app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,18 @@
+package br.com.alura.agenda.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by flavio-ss on 16/02/2018.
+ */
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Chegou SMS :D", Toast.LENGTH_SHORT).show();
+    }
+}
