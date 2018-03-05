ebb87e2e606acc1fba3fa711a1d2fd25357e1313
flavio.santana <9090flavio@gmail.com>
2018-03-05 19:43:56 -0300

    Convertendo json para aluno.

--- app/src/main/java/br/com/alura/agenda/retrofit/AgendaMessagingService.java
+++ app/src/main/java/br/com/alura/agenda/retrofit/AgendaMessagingService.java
@@ -2,10 +2,15 @@ package br.com.alura.agenda.retrofit;
 
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.IOException;
 import java.util.Map;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.dto.AlunoSync;
 
 /**
  * Created by f1avi on 28/02/2018.
@@ -19,6 +24,20 @@ public class AgendaMessagingService extends FirebaseMessagingService {
 
         Map<String, String> mensagem = remoteMessage.getData();
         Log.i("Msg recebida: ", String.valueOf(mensagem));
+        converterParaAlunos(mensagem);
+    }
 
+    private void converterParaAlunos(Map<String, String> mensagem) {
+        final String ALUNO_SYNC_KEY = "alunoSync";
+        if(mensagem.containsKey(ALUNO_SYNC_KEY)){
+            String json = mensagem.get(ALUNO_SYNC_KEY);
+            try {
+                AlunoSync alunoSync = new ObjectMapper().readValue(json, AlunoSync.class);
+                AlunoDAO dao = new AlunoDAO(this);
+                dao.inserir(alunoSync.getAlunos());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
